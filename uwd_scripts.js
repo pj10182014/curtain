143afff9c1c70e13856ea1f01b601c7aefd40cf3
ET-Air <pj10182014@gmail.com>
2015-08-11 16:36:47 -0700

    color picker still not done

--- uwd_scripts.js
+++ uwd_scripts.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
         var sideBtotal = singlePrice($('.dimensions-side-b input'));
         var sideCtotal = singlePrice($('.dimensions-side-c input'));
         var sideDtotal = singlePrice($('.dimensions-side-d input'));
-        var sideABCDtotal = (sideAtotal + sideBtotal + sideCtotal +sideDtotal);
-        $('.price').empty().append(sideABCDtotal);
+        var sideABCDtotal = (sideAtotal + sideBtotal + sideCtotal +sideDtotal).toFixed(2);
+        $('.price').empty().append("$" + sideABCDtotal);
     });
 
     /*Function to gets the price for single Side*/
@@ -32,24 +32,54 @@ $(document).ready(function(){
             return total;
     }
 
-    $('div.color1 div.color2 div.color3').hide();
+    $('div.color1 div.color2 div.color3').hide();  //hide number of colors until a value is selected
 
     /* Open the color selection field after number of colors is selected */
     $('.number-of-colors').on('change', function(){
         var $clicked = $(this);
         var $closestDiv = $clicked.closest("div");
 
-        var value = $clicked.val();
+        var chooseColorValue = $clicked.val();
 
-        if(value == 1){
+        if(chooseColorValue == 1){
             $closestDiv.find('div.color1').show().css({"width": "inherit", "height": "100%", "background-color": "pink", "border": "1px solid lightgrey", "border-radius": "5px"});
             $closestDiv.find('div.color2').hide();
             $closestDiv.find('div.color3').hide();
-        }else if(value == 2){
+
+
+            //$closestDiv.find('div.color1').on('click', function(){
+            //    $('.colorSelectBox').css({"left": "100px","top": "570px"}).toggle();
+            //});
+            //
+            //$('div.black')
+            //    .add('div.pink')
+            //    .add('div.yellow')
+            //    .on('click', function(){
+            //    var attrValue = $(this).attr('value');
+            //    $closestDiv.find('div.color1').css({"background-color": attrValue});
+            //});
+
+        }else if(chooseColorValue == 2){
             $closestDiv.find('div.color1').show().css({"width": "inherit", "height": "50%", "background-color": "pink", "border": "1px solid lightgrey", "border-radius": "5px"});
             $closestDiv.find('div.color2').show().css({"width": "inherit", "height": "50%", "background-color": "pink", "border": "1px solid lightgrey", "border-radius": "5px"});
             $closestDiv.find('div.color3').hide();
-        }else if(value == 3){
+
+            $(document).on('click', 'div.number-of-color-field > div', function(){
+                $('.colorSelectBox').css({"left": "100px","top": "570px"}).toggle();
+            });
+
+            // Bind click event to each color box
+            $(document).on('click', 'div.colorSelectBox > div > div', function() {
+                // Empty parent
+                //$(this).parent().empty();
+
+                console.log($(this).attr('value'));
+                // Assign colour to color field
+                $('div.number-of-color-field > div.' + $(this).attr('data-target')).css('background-color', $(this).attr('value'));
+            });
+
+
+        }else if(chooseColorValue == 3){
             $closestDiv.find('div.color1').show().css({"width": "inherit", "height": "33%", "background-color": "pink", "border": "1px solid lightgrey", "border-radius": "5px"});
             $closestDiv.find('div.color2').show().css({"width": "inherit", "height": "33%", "background-color": "pink", "border": "1px solid lightgrey", "border-radius": "5px"});
             $closestDiv.find('div.color3').show().css({"width": "inherit", "height": "33%", "background-color": "pink", "border": "1px solid lightgrey", "border-radius": "5px"});
@@ -60,6 +90,8 @@ $(document).ready(function(){
         }
     });
 
-    $('.colorSelectBox').hide();
+    //$('.colorSelectBox').hide();
+
+
 
 });
